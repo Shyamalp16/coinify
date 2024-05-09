117334ec3d0a900c7111e0d7f366b18b847db35a
Shyamal <patelshyamal016@gmail.com>
2024-05-08 20:44:17 -0400

    Setup Localdatabse (MySql Workbench) and Prisma connection with a temp table users

--- src/app/site/layout.tsx
+++ src/app/site/layout.tsx
@@ -1,12 +1,17 @@
 import Navigation from '@/components/site/navigation'
 import React from 'react'
+import { ClerkProvider } from "@clerk/nextjs"
+import { dark } from "@clerk/themes"
+
 
 const AuthLayout = ({children}: {children: React.ReactNode}) => {
   return (
-    <div className='h-full'>
-        <Navigation />
-        {children}
-    </div>
+    <ClerkProvider appearance={{baseTheme: dark}}>
+      <div className='h-full'>
+          <Navigation />
+          {children}
+      </div>
+    </ClerkProvider>
   )
 }
 
--- src/middleware.tsx
+++ src/middleware.tsx
@@ -1,9 +1,35 @@
 import { authMiddleware, clerkMiddleware } from "@clerk/nextjs/server";
+import { NextResponse } from "next/server";
 
 export default authMiddleware({
     publicRoutes:["/site"],
+    beforeAuth(auth, req) {},
+    afterAuth(auth, req) {
+      const url = req.nextUrl;
+      const searchParams = url.searchParams.toString();
+      let hostname = req.headers
+      const pathWithSearchParams = `${url.pathname}${searchParams.length>0?`?${searchParams}`:''}`
+
+      const customSubDomain = hostname.get('host')?.split(`${process.env.NEXT_PUBLIC_DOMAIN}`).filter(Boolean)[0];
+
+      if(customSubDomain){
+        return NextResponse.rewrite(new URL(`/${customSubDomain}${pathWithSearchParams}`))
+      }
+
+      if(url.pathname === "/sign-in" || url.pathname === "/sign-up"){
+        return NextResponse.redirect(new URL(`/agency/sign-in`, req.url))
+      }
+
+      if(url.pathname === "/" || url.pathname === "/site" && url.host === process.env.NEXT_PUBLIC_DOMAIN){
+        return NextResponse.rewrite(new URL('/site', req.url))
+      }
+
+      if(url.pathname.startsWith('/agency') || url.pathname.startsWith('/dashboard')){
+        return NextResponse.rewrite(new URL(`${pathWithSearchParams}`, req.url))
+      }
+    },
 });
 
 export const config = {
-  matcher: ["/((?!.+.[w]+$|_next).*)", "/", "/(api|trpc)(.*)"],
+  matcher: ["/((?!.+\\.[\\w]+$|_next).*)","/","/(api|trpc)(.*)"],
 };
